3b85d41a9eb00e08608b75fc0e8074566e1a9260
João Faustino <joao.faustino@mindera.com>
2021-02-19 09:23:01 +0000

    Organize directory structure and add js array exercises

---
+++ solutions/playing-with-js/src/arrays/arrays.js
@@ -0,0 +1,130 @@
+/**
+ * Add the given item to the array
+ */
+const add = (arr, item) => {
+
+};
+
+/**
+ * Add the given item to the start of the array
+ */
+const prepend = (arr, item) => {
+
+};
+
+/**
+ * Remove the given item from the array
+ */
+const remove = (arr, item) => {
+
+};
+
+/**
+ * Find the index of the given item within the given array
+ */
+const findIndex = (arr, item) => {
+
+};
+
+/**
+ * Sums all the items of an array
+ */
+const sum = (arr) => {
+
+};
+
+/**
+ * Concatenate the two arrays
+ */
+const concat = (arr1, arr2) => {
+
+};
+
+/**
+ * Compare the two arrays' content for equality
+ */
+const isEqual = (arr1, arr2) => {
+
+};
+
+/**
+ * Reverse the given array
+ */
+const reverse = (arr) => {
+
+};
+
+/**
+ * Insert an item in the array at the specific position
+ */
+const insert = (arr, item, index) => {
+
+};
+
+/**
+ * Fill a new created array with the given length with the given item
+ */
+const fill = (length, item) => {
+
+};
+
+/**
+ * Remove all the falsy values from the given array
+ */
+const compact = (arr) => {
+
+};
+
+/**
+ * Count the number of occurrences of an item in the given array
+ */
+const count = (arr, item) => {
+
+};
+
+/**
+ * Multiply all the items on the array by the given number
+ */
+const multiply = (arr, number) => {
+
+};
+
+/**
+ * Find all the duplicated items in the given array
+ */
+const findDuplicates = (arr) => {
+
+};
+
+/**
+ * Find all the occurrences of a given item on the array
+ */
+const findAllOccurrences = (arr, item) => {
+
+};
+
+/**
+ * Parse the params from the given url
+ */
+const parseParams = (url) => {
+
+};
+
+module.exports = {
+    add,
+    prepend,
+    remove,
+    findIndex,
+    sum,
+    concat,
+    isEqual,
+    reverse,
+    insert,
+    fill,
+    compact,
+    count,
+    multiply,
+    findDuplicates,
+    findAllOccurrences,
+    parseParams,
+};
